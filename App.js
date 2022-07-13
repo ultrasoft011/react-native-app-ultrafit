d0ad12b186570ef81cc93e79639f718541154186
a-ballesteros <a.ballesteros@globant.com>
2022-07-13 17:01:41 -0500

    Desafio 2

--- App.js
+++ App.js
@@ -1,21 +1,122 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, TouchableOpacity } from "react-native-web";
+import {
+  ImageBackground,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
-import { StatusBar } from 'expo-status-bar';
+import DATA from "./data/status";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hola Coder! Iniciando con React Native</Text>
-      <StatusBar style="auto" />
+  const firstImage = require("../ultrafit-ballesteros/assets/home-image.jpg");
+  const [statusSelected, setStatusSelected] = useState("");
+  const [buttonNext, setButtonNext] = useState("");
+
+  const Item = ({ title }) => (
+    <View>
+      <TouchableOpacity onPress={onPress}>
+        <Text style={styles.text}>{title}</Text>
+      </TouchableOpacity>
     </View>
+  );
+
+  const renderItem = ({ item }) => <Item title={item.title} />;
+
+  const onPress = (e) => {
+    console.log(e.target.innerText);
+    DATA.map((element) => {
+      if (e.target.innerText === element.title) {
+        setStatusSelected(element.emoticon);
+        setButtonNext(
+          <TouchableOpacity style={styles.nextButton}>Next</TouchableOpacity>
+        );
+      }
+    });
+    console.log(DATA);
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={firstImage}
+        style={styles.image}
+        imageStyle={{ opacity: 0.55 }}
+      >
+        <View>
+          <Text style={styles.header}>¿Cómo te sientes hoy?</Text>
+          <FlatList
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+          ></FlatList>
+          <Text style={styles.emoticon}>{statusSelected}</Text>
+          {buttonNext}
+        </View>
+
+        <View>
+          <Text style={styles.footer}>UltraFit</Text>
+        </View>
+
+        <StatusBar style="auto" />
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    color: "white",
+    fontSize: 23,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingTop: 100,
+    paddingBottom: 80,
+  },
+  text: {
+    color: "white",
+    fontSize: 17,
+    lineHeight: 50,
+    textAlign: "center",
+  },
+  image: {
+    flex: 1,
+    width: "100%",
+  },
+  emoticon: {
+    fontSize: 50,
+    alignSelf: "center",
+    paddingTop: 25,
+    paddingBottom: 30,
+  },
+  nextButton: {
+    alignSelf: "center",
+    alignItems: "center",
+    backgroundColor: "#6B1CB0",
+    fontSize: 17,
+    color: "white",
+    fontWeight: "bold",
+    width: "30%",
+    padding: 8,
+    opacity: 0.9,
+  },
+  footer: {
+    color: "white",
+    backgroundColor: "#6B1CB0",
+    fontSize: 14,
+    padding: 7,
+    position: "fixed",
+    left: 0,
+    bottom: 0,
+    width: "100%",
+    textAlign: "center",
   },
 });
---
+++ data/status.js
@@ -0,0 +1,19 @@
+const DATA = [
+  {
+    id: "01",
+    title: "Con buena energía, motivado",
+    emoticon: "💪",
+  },
+  {
+    id: "02",
+    title: "Un poco cansado pero animado",
+    emoticon: "🚶‍♂️",
+  },
+  {
+    id: "03",
+    title: "Cansado sin energía",
+    emoticon: "😐",
+  },
+];
+
+export default DATA;
